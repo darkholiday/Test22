d690c80f53bcae656bb75d9a19881dfeea5785ce
darkholiday <nidhal.enjoyneering@gmail.com>
2016-06-05 22:37:38 +0100

    gg

--- app/src/main/java/com/mcube/test/MainActivity.java
+++ app/src/main/java/com/mcube/test/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("test","test2");
-        Log.e("test","test2");
 
 
 
